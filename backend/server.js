78cb5d5133663e7b541c34c49908f0002b07f061
Aradhya Patro <aradhyapatro14@gmail.com>
2022-03-03 19:33:49 +0530

    axios testing done and verified,concorrently running setup done and shifted to import syntax from require syntax

--- backend/server.js
+++ backend/server.js
@@ -1,10 +1,12 @@
-const express = require("express");
-const app = express();
-const products = require("./data/products");
-const cors = require("cors");
+import dotenv from "dotenv";
+import express from "express";
+import products from "./data/products.js";
+import cors from "cors";
 
+const app = express();
 app.use(express.json());
 app.use(cors());
+dotenv.config();
 
 app.get("/", (req, res) => {
   res.send("API End Point Working");
@@ -19,6 +21,12 @@ app.get("/api/products/:id", (req, res) => {
   res.json(item);
 });
 
-app.listen(5000, () => {
-  console.log("API SERVER running");
+app.post("/testing", () => {
+  console.log("Testing successfull");
+});
+
+app.listen(process.env.PORT || 5000, () => {
+  console.log(
+    `API SERVER running ${process.env.NODE_ENVIRONMENT} environment at port = ${process.env.PORT}`
+  );
 });
